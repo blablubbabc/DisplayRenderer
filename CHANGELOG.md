86f29af616e789d1c032a91cf164de3759d06cfe
blablubbabc <lukas@wirsindwir.de>
2025-02-09 15:03:19 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+Date format: (YYYY-MM-DD)  
+
+## Next release
+### Supported MC versions: xxx
+
+## v0.1 (TBA)
+### Supported MC versions: 1.21.4
+
+
---
+++ src/main/java/de/blablubbabc/displayRenderer/DisplayRendererPlugin.java
@@ -0,0 +1,14 @@
+package de.blablubbabc.displayRenderer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DisplayRendererPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
